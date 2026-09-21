441193ad23acbd920f27879045c112f49a513cad
arunhistory <garigarimegane625@gmail.com>
2026-09-22 07:23:41 +0900

    Build AT bonus and CZ state modules into slot v2 WASM

--- tools/build-wasm-v2.sh
+++ tools/build-wasm-v2.sh
@@ -62,6 +62,9 @@ SRC="$ROOT/src/slot-v2"
   "$SRC/acquisition/index.cpp" \
   "$SRC/normal-progress/index.cpp" \
   "$SRC/runtime/index.cpp" \
+  "$SRC/cz-state/index.cpp" \
+  "$SRC/bonus-state/index.cpp" \
+  "$SRC/at-state/index.cpp" \
   "$SRC/stock/index.cpp" \
   "$SRC/section/index.cpp" \
   "$SRC/line/index.cpp" \
