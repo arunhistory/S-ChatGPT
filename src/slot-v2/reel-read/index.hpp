10f8098047bc2f3b95d98421e45c994d2cb1df1c
arunhistory <garigarimegane625@gmail.com>
2026-09-22 06:31:29 +0900

    Add slot v2 program: src/slot-v2/reel-read/index.hpp

---
+++ src/slot-v2/reel-read/index.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "../shared/types.hpp"
+
+namespace slotv2::reel_read {
+
+Symbol at(ReelId reel, uint8_t position);
+Symbol visible(ReelId reel, uint8_t center_position, int8_t row_offset);
+
+} // namespace slotv2::reel_read
