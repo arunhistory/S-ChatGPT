b155e5cfa32e5377fd54c22a5fc0ad7323bbc33f
arunhistory <garigarimegane625@gmail.com>
2026-09-22 16:43:31 +0900

    Require bonus state exports

--- src/ts/slot/wasm/load.ts
+++ src/ts/slot/wasm/load.ts
@@ -35,6 +35,8 @@ const REQUIRED_EXPORTS = [
   "slot_v2_at_window",
   "slot_v2_section_transition",
   "slot_v2_special_zone",
+  "slot_v2_bonus_cycle",
+  "slot_v2_bonus_state",
   "slot_v2_normal_mode",
   "slot_v2_ceiling_at",
   "slot_v2_ceiling_count",
