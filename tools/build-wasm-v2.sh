d1491cfb8d781885c6e6c5923d56c1002dc8f54a
arunhistory <garigarimegane625@gmail.com>
2026-09-22 22:27:05 +0900

    Compile upper special modules into WASM

--- tools/build-wasm-v2.sh
+++ tools/build-wasm-v2.sh
@@ -140,6 +140,8 @@ SRC="$ROOT/src/slot-v2"
   "$SRC/special-zone-pending/index.cpp" \
   "$SRC/upper-special/index.cpp" \
   "$SRC/upper-special-transition/index.cpp" \
+  "$SRC/upper-special/index.cpp" \
+  "$SRC/upper-special-transition/index.cpp" \
   "$SRC/upper-comeback/index.cpp" \
   "$SRC/upper-comeback-cycle/index.cpp" \
   "$SRC/revival-cycle/index.cpp" \
