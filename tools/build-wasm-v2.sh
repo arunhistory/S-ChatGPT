d0e3acc919c0a93c05826d78915b863f9b3903e7
arunhistory <garigarimegane625@gmail.com>
2026-09-22 18:02:24 +0900

    Export special-zone result from slot v2 WASM

--- tools/build-wasm-v2.sh
+++ tools/build-wasm-v2.sh
@@ -21,6 +21,7 @@ SRC="$ROOT/src/slot-v2"
   -Wl,--export=slot_v2_at_window \
   -Wl,--export=slot_v2_section_transition \
   -Wl,--export=slot_v2_special_zone \
+  -Wl,--export=slot_v2_special_zone_result \
   -Wl,--export=slot_v2_cz_cycle \
   -Wl,--export=slot_v2_cz_finalize \
   -Wl,--export=slot_v2_normal_display_games \
