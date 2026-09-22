fe8808b1bfc94814fb89ecebf07590ef2ef6ef8d
arunhistory <garigarimegane625@gmail.com>
2026-09-23 08:24:51 +0900

    Orchestrate normal high state before raw lotteries

---
+++ src/slot-v2/normal-flow/index.cpp
@@ -0,0 +1,49 @@
+#include "index.hpp"
+
+namespace slotv2::normal_flow {
+
+Result draw(
+    Rng& rng,
+    normal_high::State& high,
+    RoleFlag role,
+    uint32_t actual_games,
+    uint8_t setting
+) {
+    Result out{};
+
+    out.high = normal_high::play(
+        rng,
+        high,
+        role,
+        actual_games
+    );
+
+    if (out.high.reward == normal_high::Reward::CZ) {
+        out.reward = Reward::CZ;
+        return out;
+    }
+
+    if (out.high.reward == normal_high::Reward::Bonus) {
+        out.reward = Reward::Bonus;
+        return out;
+    }
+
+    switch (normal_raw_lottery::draw(rng, setting)) {
+        case normal_raw_lottery::Reward::LowerAT:
+            out.reward = Reward::LowerAT;
+            break;
+        case normal_raw_lottery::Reward::Bonus:
+            out.reward = Reward::Bonus;
+            break;
+        case normal_raw_lottery::Reward::CZ:
+            out.reward = Reward::CZ;
+            break;
+        case normal_raw_lottery::Reward::None:
+        default:
+            break;
+    }
+
+    return out;
+}
+
+} // namespace slotv2::normal_flow
