a215badb2902792b44bfff4d1212ffe783734f48
arunhistory <garigarimegane625@gmail.com>
2026-09-22 22:25:52 +0900

    Add upper special zone state

---
+++ src/slot-v2/upper-special/index.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <stdint.h>
+#include "../shared/rng.hpp"
+
+namespace slotv2::upper_special {
+
+struct State {
+    bool active{false};
+    uint32_t rounds{0};
+    uint32_t total_added_games{0};
+};
+
+struct Step {
+    bool active_before{false};
+    bool success{false};
+    bool ended{false};
+    uint16_t added_games{0};
+    uint32_t round{0};
+};
+
+void start(State& state);
+uint16_t addGamesFromRoll(uint16_t roll_0_to_99);
+Step playOne(State& state, Rng& rng);
+
+} // namespace slotv2::upper_special
