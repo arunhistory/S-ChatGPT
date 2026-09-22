4c24c94de785a693b39043388b55988045da77b4
arunhistory <garigarimegane625@gmail.com>
2026-09-22 22:39:46 +0900

    Add red seven entry stop rule header

---
+++ src/slot-v2/stop-rules/entry.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "common.hpp"
+
+namespace slotv2::stop_rules::entry {
+
+bool accepts(
+    const stop_shared::Context& ctx,
+    const reel_strip::StripView& strip,
+    uint8_t candidate
+);
+
+} // namespace slotv2::stop_rules::entry
