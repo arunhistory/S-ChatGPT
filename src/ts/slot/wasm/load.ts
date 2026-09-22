f7375ce6b48c9d7e592d84cdfda34b76e41dc6cf
arunhistory <garigarimegane625@gmail.com>
2026-09-22 15:26:34 +0900

    Require section transition and AT window exports

--- src/ts/slot/wasm/load.ts
+++ src/ts/slot/wasm/load.ts
@@ -31,6 +31,8 @@ const REQUIRED_EXPORTS = [
   "slot_v2_point_count",
   "slot_v2_pending_events",
   "slot_v2_section_reward",
+  "slot_v2_at_window",
+  "slot_v2_section_transition",
   "slot_v2_normal_mode",
   "slot_v2_ceiling_at",
   "slot_v2_ceiling_count",
