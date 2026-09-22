769610d0456b1e1bc6073487df58ddb57d44aa5d
arunhistory <garigarimegane625@gmail.com>
2026-09-23 08:22:46 +0900

    Compile normal rare role trigger into WASM

--- tools/build-wasm-v2.sh
+++ tools/build-wasm-v2.sh
@@ -139,6 +139,7 @@ SRC="$ROOT/src/slot-v2"
   "$SRC/normal-ceiling/index.cpp" \
   "$SRC/normal-ceiling-transition/index.cpp" \
   "$SRC/normal-cycle-reset/index.cpp" \
+  "$SRC/normal-role-trigger/index.cpp" \
   "$SRC/at-resolution/index.cpp" \
   "$SRC/at-single-transition/index.cpp" \
   "$SRC/section-flow/index.cpp" \
