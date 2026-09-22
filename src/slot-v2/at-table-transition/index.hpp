e74224759a7ca3595d20a4db4b4bb138c1d33231
arunhistory <garigarimegane625@gmail.com>
2026-09-22 22:30:46 +0900

    Add AT table transition lottery

---
+++ src/slot-v2/at-table-transition/index.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <stdint.h>
+#include "../shared/rng.hpp"
+#include "../at-state/index.hpp"
+
+namespace slotv2::at_table_transition {
+
+at_state::Table fromRoll(
+    at_state::Table current,
+    uint16_t roll_0_to_99
+);
+
+at_state::Table draw(
+    Rng& rng,
+    at_state::Table current
+);
+
+} // namespace slotv2::at_table_transition
