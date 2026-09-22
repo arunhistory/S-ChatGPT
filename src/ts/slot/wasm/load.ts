00eb4c4b6a2be5dcdd6d5657f9ac4958a36d999c
arunhistory <garigarimegane625@gmail.com>
2026-09-22 15:28:38 +0900

    Require special-zone export

--- src/ts/slot/wasm/load.ts
+++ src/ts/slot/wasm/load.ts
@@ -33,6 +33,7 @@ const REQUIRED_EXPORTS = [
   "slot_v2_section_reward",
   "slot_v2_at_window",
   "slot_v2_section_transition",
+  "slot_v2_special_zone",
   "slot_v2_normal_mode",
   "slot_v2_ceiling_at",
   "slot_v2_ceiling_count",
