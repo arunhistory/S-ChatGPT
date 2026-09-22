b366a0da54e39b45c85b8358910bfd04bad6e298
arunhistory <garigarimegane625@gmail.com>
2026-09-22 14:04:54 +0900

    Add unresolved AT and section events to pending bitset

--- src/slot-v2/pending-event/index.hpp
+++ src/slot-v2/pending-event/index.hpp
@@ -7,7 +7,19 @@ enum Bits : uint32_t {
     None             = 0u,
     BellFiveAT       = 1u << 0,
     CZThreeMissHit   = 1u << 1,
-    NextHitAT        = 1u << 2
+    NextHitAT        = 1u << 2,
+
+    ATHit            = 1u << 3,
+    ATFall           = 1u << 4,
+    ATAddGames       = 1u << 5,
+    ATSpecial        = 1u << 6,
+    ATEpisode        = 1u << 7,
+    ATUpperSpecial   = 1u << 8,
+    ATMultiple       = 1u << 9,
+
+    SectionTierUp    = 1u << 10,
+    SectionSpecial   = 1u << 11,
+    SectionUpperSpec = 1u << 12
 };
 
 struct State {
