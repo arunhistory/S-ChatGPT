ac572090a98b6d2c42101cf4ca2e4b58563eeea7
arunhistory <garigarimegane625@gmail.com>
2026-09-23 08:06:01 +0900

    Use regular bonus in Special mode

--- src/slot-v2/bonus-entry-policy/index.cpp
+++ src/slot-v2/bonus-entry-policy/index.cpp
@@ -7,21 +7,14 @@ Result forMode(normal_mode::Mode mode) {
         case normal_mode::Mode::Heaven:
         case normal_mode::Mode::SuperHeaven:
             return {
-                Status::Resolved,
                 bonus_state::Kind::Episode
             };
 
         case normal_mode::Mode::NormalA:
         case normal_mode::Mode::NormalB:
-            return {
-                Status::Resolved,
-                bonus_state::Kind::Regular
-            };
-
         case normal_mode::Mode::Special:
         default:
             return {
-                Status::SpecialModeUnresolved,
                 bonus_state::Kind::Regular
             };
     }
