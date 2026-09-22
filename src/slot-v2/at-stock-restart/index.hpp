d0fe0197b946444e0649cb6dcc2195d7c51ba6e6
arunhistory <garigarimegane625@gmail.com>
2026-09-22 22:30:52 +0900

    Add AT stock restart module

---
+++ src/slot-v2/at-stock-restart/index.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <stdint.h>
+#include "../shared/rng.hpp"
+#include "../machine-state/index.hpp"
+#include "../pending-event/index.hpp"
+
+namespace slotv2::at_stock_restart {
+
+struct Result {
+    bool applied{false};
+    int games{0};
+    at_state::Table before_table{at_state::Table::Normal};
+    at_state::Table after_table{at_state::Table::Normal};
+    uint32_t stock_after{0};
+};
+
+int gamesFromRoll(uint16_t roll_0_to_999);
+
+Result apply(
+    Rng& rng,
+    machine_state::State& machine,
+    pending_event::State& pending
+);
+
+} // namespace slotv2::at_stock_restart
