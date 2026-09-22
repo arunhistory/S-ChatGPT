adb30be8bd960d7a304b1b4a665fa3002a4be889
arunhistory <garigarimegane625@gmail.com>
2026-09-23 07:55:32 +0900

    Add normal ceiling reward lottery

---
+++ src/slot-v2/normal-ceiling/index.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <stdint.h>
+#include "../shared/rng.hpp"
+#include "../normal-mode/index.hpp"
+
+namespace slotv2::normal_ceiling {
+
+enum class Reward : uint8_t {
+    None = 0,
+    CZ = 1,
+    Bonus = 2,
+    LowerAT = 3,
+    LowerATWithStock = 4,
+    Freeze = 5
+};
+
+Reward fromRoll(
+    normal_mode::Mode mode,
+    uint16_t ceiling,
+    uint8_t roll_0_to_99
+);
+
+Reward draw(
+    Rng& rng,
+    normal_mode::Mode mode,
+    uint16_t ceiling
+);
+
+} // namespace slotv2::normal_ceiling
