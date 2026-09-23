e6d6dbef46b377b077da4652de60d5ed796cc7fe
arunhistory <garigarimegane625@gmail.com>
2026-09-23 13:23:58 +0900

    Store lower fall challenge state

--- src/slot-v2/cpp/core/machine_state.hpp
+++ src/slot-v2/cpp/core/machine_state.hpp
@@ -8,6 +8,7 @@
 #include "at/at_state.hpp"
 #include "at/at_omen.hpp"
 #include "at/lower_fall_challenge.hpp"
+#include "at/lower_fall_challenge.hpp"
 #include "bonus/bonus_state.hpp"
 #include "cz/cz_state.hpp"
 #include "special/special_zone.hpp"
@@ -35,6 +36,7 @@ struct State {
     normal_progress::State normal_progress{};
     at_state::State at{};
     at_omen::State at_omen{};
+    lower_fall_challenge::State lower_fall_challenge{};
     lower_fall_challenge::State lower_fall_challenge{};
     bonus_state::State bonus{};
     Area bonus_return_area{Area::Normal};
